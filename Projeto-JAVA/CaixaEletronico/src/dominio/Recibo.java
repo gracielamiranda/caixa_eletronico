421d0451102285d5975126f72e523847d9b0a9a0
amfirmo <Andre@amf-mapf>
2013-05-19 10:06:20 -0300

    remocao de caracteres nao UTF-8

--- Projeto-JAVA/CaixaEletronico/src/dominio/Recibo.java
+++ Projeto-JAVA/CaixaEletronico/src/dominio/Recibo.java
@@ -2,14 +2,14 @@ package dominio;
 
 public class Recibo {
     //TODO DEFINIR O TIPO do atributo operacao
-	//TODO DEFINIR operacao como privado e definir métodos de acesso.
+	//TODO DEFINIR operacao como privado e definir metodos de acesso.
 	//public operacao;
 	//TODO DEFINIR O TIPO do atributo data
-	//TODO DEFINIR data como privado e definir métodos de acesso.
+	//TODO DEFINIR data como privado e definir metodos de acesso.
 	//public data;
 	//TODO DEFINIR O TIPO do atributo hora
-	//TODO DEFINIR hora como privado e definir métodos de acesso.
+	//TODO DEFINIR hora como privado e definir metodos de acesso.
 	//public hora;
-	//TODO DEFINIR conta como privado e definir métodos de acesso.	
+	//TODO DEFINIR conta como privado e definir metodos de acesso.	
 	public Conta conta;
 }
